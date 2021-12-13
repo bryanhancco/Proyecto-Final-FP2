a095b7a4c5fa2bd856c7b4e077f5366b6331d635
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2021-12-12 20:01:52 -0500

    Update Libre.java

--- Libre.java
+++ Libre.java
@@ -1,13 +1,18 @@
 
 public class Libre extends Cuadrante {
     private int numero;
-
+    private boolean tieneEscudo;
+    public static final int cantEscudos = 4;
+    
     public Libre(int f, int c) {
         super(f, c);
         tieneMina = false;
     }
     public void setNumero(int num) {
         numero = num;
+    }
+    public void setEscudo(){
+        tieneEscudo = true;
     }
     public int getNumero() {
         return numero;
