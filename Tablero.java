4b2802bcd924be164a409b3e1f7d41b97e150c8a
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2021-12-12 19:13:09 -0500

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -1,4 +1,17 @@
 
 public class Tablero {
     private Cuadrante[][] miTablero;
+    
+    public void generarMinas() {
+        int[][] pos = new int[10][10];
+        int fila;
+        int columna;
+        for (int i=0; i<Mina.cantMinas; i++){
+            do {
+                fila = (int)(Math.random()*10);
+                columna = (int)(Math.random()*10);
+            } while(pos[fila][columna] != 0);
+            miTablero[fila][columna] = new Mina(fila, columna);
+        }
+    }
 }
