21acb6ab38e626eeeb950500d7e7e2ae0fad08d0
atn25042002 <93600196+atn25042002@users.noreply.github.com>
2021-12-12 19:44:13 -0500

    Base

---
+++ Soldado.java
@@ -0,0 +1,43 @@
+public class Soldado {
+	private boolean tieneEscudo;
+	private String nombre, ubicacion;
+	private int team;
+	
+	public Soldado(int team, int num, String ubicacion) {
+		nombre= "Soldado" + team;
+		this.team= team;
+		this.ubicacion= ubicacion;
+	}
+	
+	public void setEscudo() {
+		tieneEscudo= true;
+	}
+	
+	public void perderEscudo() {
+		tieneEscudo= false;
+	}
+	
+	public boolean tieneEscudo() {
+		return tieneEscudo;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public int getTeam() {
+		return team;
+	}
+	
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	
+	public String lucha(Soldado s) {
+		int r= (int)(Math.random()* 2);
+		if(r== 1)
+			return getUbicacion();
+		else
+			return s.getUbicacion();
+	}
+}
