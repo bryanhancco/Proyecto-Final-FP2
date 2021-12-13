ae829b9e546b64dcc88fc6b00b4ad86c395d7898
atn25042002 <93600196+atn25042002@users.noreply.github.com>
2021-12-12 20:03:39 -0500

    Base

---
+++ Ejercito.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ejercito {
+	private HashMap<String, Soldado> soldados= new HashMap<String, Soldado>();
+	private int team;
+	
+	public Ejercito(int c, int team) {
+		this.team= team;
+		String ub;
+		for(int f= 0; f< 10; f++) {
+			ub= Tablero.toKey(f, c);
+			soldados.put(ub, new Soldado(team, f + 1, ub));
+		}
+	}
+}
