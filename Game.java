116871322a6972577f24421d2d6aafd7541a6af9
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2021-12-15 23:09:47 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -6,18 +6,24 @@ public class Game {
     
     public Game() {
         miTablero = new Tablero();
-        ejer1 = new Ejercito(0, 1);
-        ejer2 = new Ejercito(11, 2);
+        ejer1 = new Ejercito(1, 1);
+        ejer2 = new Ejercito(12, 2);
     }
-    public void iniciarJuego() {
-    	
+    public void iniciarJuego() {  
+        do {
+            System.out.println("Turno del jugador 1");
+            jugarPorTurnos(ejer1, ejer2, miTablero);
+            System.out.println("Turno del jugador 2");
+            jugarPorTurnos(ejer2, ejer1, miTablero);        
+        } while (!ejer1.getSoldados().isEmpty() && !ejer2.getSoldados().isEmpty());
     }
     public void imprimirTablero(String pos){
         System.out.println("\n \tA\tB\tC\tD\tE\tF\tG\tH\tI\tJ\tK\tL");
         for (int i=0; i<miTablero.getCuadrantes().length; i++){
             System.out.print("\n"+(i+1)+"\t");
             for (int j=0; j<miTablero.getCuadrantes()[0].length; j++){
-                if (miTablero.getCuadrantes()[i][j].equals(pos)) 
+                if (miTablero.getCuadrantes()[i][j] == null) continue;
+                if (Tablero.toKey(i+1, j+1).equals(pos)) 
                     System.out.print("S\t");     
                 else if (miTablero.getCuadrantes()[i][j].getEstadoOculto())
                     System.out.print("-\t");
@@ -35,39 +41,46 @@ public class Game {
         int fil = Integer.parseInt(posSoldado.substring(0, 1));
         int col = posSoldado.substring(1).compareTo("A");
         for (int i=fil-1; i<=fil+1; i++){
-            if (i < 0 || i > 9) continue;
+            if (i <= 0 || i > 9) continue;
             for (int j=col-1; j<=col+1; j++){
-                if (j < 0 || j > 11) continue;
+                if (j <= 0 || j > 11) continue;
                 if (i == fil && j == col) continue; 
-                movValidas.add(fil+"ABCDEFGHIJKL".substring(col, col+1));
+                movValidas.add((i+1)+"ABCDEFGHIJKL".substring(j, j+1));
             }
         }
         return movValidas;
     }
-    public static void jugarPorTurnos(Ejercito ejerJug, Ejercito ejerEnem, Tablero miT){
+    public void jugarPorTurnos(Ejercito ejerJug, Ejercito ejerEnem, Tablero miT){
         Scanner scan = new Scanner(System.in);
         int cont = 0;
         String pInicial = "", pMovimiento, nomReino;
-        int fila, columna;
+        int fila; 
+        String columna;
         for (String key : ejerJug.getSoldados().keySet()){
             if (cont == 0){
+                System.out.println(key);
                 pInicial = key;
                 cont++;
             }
         }
-        System.out.println("A que posición quiere moverlo");
+        imprimirTablero(pInicial);
+        System.out.println("\nA que posición quiere moverlo");
         System.out.println("Fila");
         fila = scan.nextInt();
         System.out.println("Columna");
-        columna = scan.nextInt();
-        pMovimiento = fila+"ABCDEFGHIJKL".substring(columna, columna+1);
+        columna = scan.next();
+        pMovimiento = fila+columna;
+        for (String p : movimientosValidos(pInicial)){
+            System.out.println(p);
+        }
         while(!movimientosValidos(pInicial).contains(pMovimiento)){
             System.out.println("No es posible moverlo a esa ubicación");
             System.out.println("A que posición quiere moverlo");
             System.out.println("Fila");
             fila = scan.nextInt();
             System.out.println("Columna");
-            columna = scan.nextInt();
+            columna = scan.next();
+            pMovimiento = fila+columna;
         }
         mover(pInicial, miT, pMovimiento, ejerJug.getSoldados());
     }
@@ -88,3 +101,4 @@ public class Game {
         }
     }
 }
+
