3c2cd8c6e86a8e0202421be3a294a6f0cab25f12
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2021-12-17 09:09:02 -0500

    Nueva clase para crear torres

---
+++ Torre.java
@@ -0,0 +1,20 @@
+
+public class Torre {
+    private int vidaTorre;
+    private boolean torreEnPie;
+    
+    public Torre() {
+        vidaTorre = 5;
+        torreEnPie = true;
+    }
+    public void torreAtacada() {
+        vidaTorre--;
+    }
+    public void torreDestruida() {
+        System.out.println("La torre ha sido destruida");
+        torreEnPie = false;
+    }
+    public int getVidaTorre() {
+        return vidaTorre;
+    }
+}
