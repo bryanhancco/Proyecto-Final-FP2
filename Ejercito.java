067430bd2310843e92373f16f7e51ff343f8c788
elmer <elmer@DESKTOP-5HTHOJI>
2021-12-21 18:30:06 -0500

    Ejercito-modificado

--- Ejercito.java
+++ Ejercito.java
@@ -7,11 +7,11 @@ public class Ejercito {
     public Ejercito(int c, int team) {
         miTorre = new Torre();
         this.team= team;
-	String ub;
-	for(int f= 0; f< 10; f++) {
+		String ub;
+		for(int f= 0; f< 10; f++) {
             ub= Tablero.toKey(f+1, c);
             soldados.put(ub, new Soldado(team, f + 1, ub));
-	}
+		}
     }
 	
     public int getTeam(){
