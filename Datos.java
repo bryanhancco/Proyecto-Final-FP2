4bad2761495a4ee64f4b73653276b4d0dab25bb1
elmer <elmer@DESKTOP-5HTHOJI>
2021-12-29 13:18:19 -0500

    Agregando la interfaz

---
+++ Datos.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+interface Datos {
+	Color c1 = null;
+	Color c2 = null;
+	String nombre1= "", nombre2= "";
+	
+	final Color[] COLORES= {Color.YELLOW, Color.RED, Color.GREEN, Color.BLUE
+			, Color.MAGENTA, Color.PINK, Color.ORANGE};
+	final String[] colores= {"Amarrillo", "Rojo", "Green", "Blue"
+			, "Magenta", "Rosa", "Naranja"};
+}
