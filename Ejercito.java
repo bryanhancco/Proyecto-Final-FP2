89c8518bba4c7b5708c357c53138b36208965cd2
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2021-12-30 17:10:55 -0500

    Correcciones en la identacion

--- Ejercito.java
+++ Ejercito.java
@@ -1,17 +1,18 @@
 import java.util.*;
+
 public class Ejercito {
-    private HashMap<String, Soldado> soldados= new HashMap<String, Soldado>();
+    private HashMap<String, Soldado> soldados = new HashMap<String, Soldado>();
     private Torre miTorre;
     private int team;
 	
     public Ejercito(int c, int team) {
         miTorre = new Torre();
         this.team= team;
-		String ub;
-		for(int f= 0; f< 10; f++) {
+	String ub;
+	for(int f= 0; f< 10; f++) {
             ub= Tablero.toKey(f+1, c);
             soldados.put(ub, new Soldado(team, f + 1, ub));
-		}
+        }
     }
 	
     public int getTeam(){
@@ -28,23 +29,23 @@ public class Ejercito {
         ArrayList<Soldado> lst= new ArrayList<Soldado>(); 
 	for (Soldado s: soldados.values())
             lst.add(s);
-            for(int i= 0; i< lst.size()- 1; i++)
-		for(int j= 0; j< lst.size()- 1; j++)
-			if(lst.get(j).getNumero() > lst.get(j + 1).getNumero()){
-				Soldado aux= lst.get(j);
-				lst.set(j, lst.get(j + 1));
-				lst.set(j + 1, aux);
-			}
+        for(int i = 0; i< lst.size()- 1; i++) {
+            for(int j= 0; j< lst.size()- 1; j++) {
+                if(lst.get(j).getNumero() > lst.get(j + 1).getNumero()){
+                    Soldado aux= lst.get(j);
+                    lst.set(j, lst.get(j + 1));
+                    lst.set(j + 1, aux);
+                }
+            }
+        }
 	return lst;		
     }
     
     public void retirarSoldado(String k) {
-    	System.out.println("---> Retirando " + k + " : " + soldados.get(k).getNombre());
-    	soldados.remove(k);
-    }
-    
+        soldados.remove(k);
+    }  
     public void moverSoldado(String k1, String k2) {
-    	soldados.get(k1).setUbicacion(k2);
-    	soldados.put(k2, soldados.remove(k1));
-    }
+        soldados.get(k1).setUbicacion(k2);
+        soldados.put(k2, soldados.remove(k1));
+    }    
 }
