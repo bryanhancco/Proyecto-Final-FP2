82122e270d34f7be9456398ab90dbd58e8ac74fb
atn25042002 <93600196+atn25042002@users.noreply.github.com>
2021-12-30 18:27:03 -0500

    agregando arreglos

--- Datos.java
+++ Datos.java
@@ -3,16 +3,22 @@ interface Datos {
 	Color c1 = null;
 	Color c2 = null;
 	String nombre1= "", nombre2= "";
-	
+
+    String[] modos= {"Clásico","Turbo","Realista"};
 	final Color[] COLORES= {Color.YELLOW, Color.RED, Color.GREEN, Color.BLUE
 			, Color.MAGENTA, Color.PINK, Color.ORANGE};
 	final String[] colores= {"Amarrillo", "Rojo", "Green", "Blue"
 			, "Magenta", "Rosa", "Naranja"};
-	String[] consejos= {"Observa indicador de n�mero" + "\nde minas antes de mover",
+	String[] consejos= {"Observa indicador de número" + "\nde minas antes de mover",
 			"",
 			"",
 			"",
 			"",
 			"",
 			""};
+	
+	String[] ayudas= 
+		{"Gana el que lleve cinco soldados a la \nfortaleza enemiga",
+			"Similar al clásico pero solo es necesario \nllevar un soldado",
+			"Los soldados ya no reaparecen y se \npuede perder por falta de unidades"};
 }
