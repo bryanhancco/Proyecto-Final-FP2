e29b0f17726f3362f0ec91bb0c73575076da943a
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2021-12-31 15:11:14 -0500

    Posible versión final de Run And Destroy

--- RunAndDestroy.java
+++ RunAndDestroy.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
+
 public class RunAndDestroy {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String conf;
-        Cuestionario inicio = new Cuestionario();
+        new Menu();
     }
 }
