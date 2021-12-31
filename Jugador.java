fb1cead7c7347578c1d47c4bb53dfc1aeee06667
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2021-12-31 17:34:46 -0500

    Traspasando los métodos de DatosJugador, version final

--- Jugador.java
+++ Jugador.java
@@ -1,6 +1,6 @@
+
 import java.awt.*;
 import java.io.*;
-
 public class Jugador implements Serializable {
     private int cantSoldados;
     private Color colorEscogido;
@@ -55,4 +55,34 @@ public class Jugador implements Serializable {
         
         return puntajeFinal;
     }
+    public static void actualizarDatos(Jugador j1, Jugador j2){
+        ObjectOutputStream fileOut;
+        try {
+            fileOut = new ObjectOutputStream(new FileOutputStream("ultimaPartida.dat"));
+            fileOut.writeObject(j1);
+            fileOut.writeObject(j2);
+            fileOut.close();
+        } 
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }   
+    public static Jugador[] obtenerDatosJugador () {
+        Jugador[] jugadores = new Jugador[2];
+        ObjectInputStream fileIn;
+        try {
+            fileIn = new ObjectInputStream(new FileInputStream("ultimaPartida.dat"));
+            jugadores[0] = (Jugador) fileIn.readObject();
+            jugadores[1]  = (Jugador) fileIn.readObject();
+            fileIn.close();
+        } 
+        catch (IOException e) {
+            System.out.println("IO Error: " + e.getMessage());
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFound: " + e.getMessage());
+        }
+        return jugadores;
+    }
 }
+
